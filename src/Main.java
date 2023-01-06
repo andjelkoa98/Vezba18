ee3a41d6b6789854506d1d880b5330a8ce0293d7
Your Name <andjelkoa@gmail.com>
2023-01-06 10:46:48 +0100

    final version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String [] imena = new String[]{"Heart", "Cherry", "Coin", "Melon", "Jocker"};
+        Random rand = new Random();
+        int rendomBroj;
+        String [] noviNiz = new String[imena.length];
+
+        for (int i = 0; i < imena.length; i++) {
+            rendomBroj= rand.nextInt(imena.length);
+            noviNiz[i] = imena[rendomBroj];
+        }
+        for (String i : noviNiz) {
+            System.out.print(i + " ");
+        }
     }
 }
